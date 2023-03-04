5e40c0af81fa26b77ba65e25ea5e8b9d24245892
mgirshfeldbooth <mgirshfeld@chicagobooth.edu>
2023-03-04 00:18:35 +0000

    Added navbar and alert messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  root "rounds#index"
+
+  devise_for :users
+
   resources :attempts
   resources :rounds
   resources :plans
   resources :subjects
   resources :exercises
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
 end
