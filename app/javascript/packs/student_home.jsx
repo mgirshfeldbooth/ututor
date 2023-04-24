545ec1f6f90e6cfa37d382e0e8487cab173ee1bf
mgirshfeldbooth <mgirshfeld@chicagobooth.edu>
2023-04-24 02:47:34 +0000

    just messing around

--- app/javascript/packs/student_home.jsx
+++ app/javascript/packs/student_home.jsx
@@ -232,7 +232,7 @@ function PracticePlan({
           ) : (
             <Card>
               <CardTitle>Ready to play?</CardTitle>
-              <Subtitle>You will do {numberOfQuestions} exercise(s)</Subtitle> 
+              <Subtitle>You will do {numberOfQuestions} exercise(s). Go as FAST as you can!</Subtitle> 
               <Button onClick={handleStartPractice}>Go!</Button>
             </Card>
           )}
